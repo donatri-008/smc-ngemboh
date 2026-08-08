cf054e642e49dd186a9996d9d4abaf08508412c0
NailaFitriaa <149094372+NailaFitriaa@users.noreply.github.com>
2026-08-08 11:10:59 +0700

    Update partners.blade.php

--- resources/views/about/partials/partners.blade.php
+++ resources/views/about/partials/partners.blade.php
@@ -1,6 +1,6 @@
-<div>
-    <h2 class="text-xl font-bold text-gray-700 mb-6">Mitra</h2>
-    <div class="grid grid-cols-2 md:grid-cols-5 gap-6">
+<div class="max-w-5xl mx-auto">
+    <h2 class="text-xl font-bold text-gray-700 mb-6 text-center">Mitra</h2>
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
         @forelse($partners as $partner)
         <x-ui.card padding="p-4" class="text-center space-y-2">
             <img src="{{ Storage::url($partner->logo) }}" class="w-16 h-16 object-contain mx-auto shadow-neu-in rounded-lg p-2 bg-white">
