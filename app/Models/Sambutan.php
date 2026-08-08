2d7c484082365d198d48682fd9f9a37a12e7cc09
NailaFitriaa <149094372+NailaFitriaa@users.noreply.github.com>
2026-08-08 10:42:18 +0700

    Add files via upload

---
+++ app/Models/Sambutan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sambutan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nama', 'jabatan', 'foto', 'isi_sambutan', 'urutan'];
+}
